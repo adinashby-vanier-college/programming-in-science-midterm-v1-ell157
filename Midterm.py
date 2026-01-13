fc67ba8374eb487bddd1a8c20478386053854106
ell157 <ellispacquing@gmail.com>
2026-01-13 10:59:21 -0500

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -2,7 +2,9 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+   area = round((math.pi * radius ** 2), 2)
+   
+   return area
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
